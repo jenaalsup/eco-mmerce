f77bef2f3489956869d97745c3350fe6e0da53bc
jenaalsup <jena.alsup@gmail.com>
2023-08-27 15:31:30 -0700

    add homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Shop
+            <br classname="max-md:hiden" />
+            <span className="orange_gradient text-center">Sustainable Styles</span>
+        </h1>
+        <p className="desc text-center">
+            Eco-mmerce is an online shopping hub that markets a curated list of
+            clothing items from verified sustainable brands. Build a more 
+            sustainable future with Eco-mmerce.
+        </p>
+
+        {/* Feed */}
+    </section>
   )
 }
 
